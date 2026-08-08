d833aae7e5b28df1adfdb4332767c8efad9c2966
M Mudasir Iqbal <mr.mudasiriqbal.developer@gmail.com>
2026-08-08 16:04:32 +0500

    Time: 12 ms (51.32%), Space: 29.4 MB (24.3%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.cpp
+++ 2418-sort-the-people/2418-sort-the-people.cpp
@@ -1,17 +1,37 @@
 class Solution {
 public:
+    // with O(N^2)
+    // vector<string> sortPeople(vector<string>& names, vector<int>& h) {
+        
+    //     int n = h.size();
+    //     for(int i=0; i<n; i++) {
+    //         for(int j=i+1; j<n; j++) {
+    //             if (h[j] > h[i]) {
+    //                 swap(h[i], h[j]);
+    //                 swap(names[i], names[j]);
+                    
+    //             }
+    //         }
+    //     }
+    //     return names;
+    // }
+
+
+    // optimal solution with O(NlogN);
     vector<string> sortPeople(vector<string>& names, vector<int>& h) {
         
         int n = h.size();
+        vector<pair<int, string>> people(n);
         for(int i=0; i<n; i++) {
-            for(int j=i+1; j<n; j++) {
-                if (h[j] > h[i]) {
-                    swap(h[i], h[j]);
-                    swap(names[i], names[j]);
-                    
-                }
-            }
+            people[i] = {h[i], names[i]};
         }
+
+        sort(people.rbegin(), people.rend());
+        for(int i=0; i<n; i++) {
+            names[i] = people[i].second;
+        }    
+        
         return names;
     }
+
 };
